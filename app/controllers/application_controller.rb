60d3c6f5f049cc08b97358ff3509d83f78e39b20
Lauren McCoy <lmccoy8911@gmail.com>
2019-10-06 00:29:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
       redirect '/failure' 
     elsif user && user.authenticate(params[:password])
       session[user_id] = user.id 
-      redirect '/login'
+      erb :login 
     end 
 
   end
