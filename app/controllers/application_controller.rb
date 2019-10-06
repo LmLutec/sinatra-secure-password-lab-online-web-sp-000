8603e565c95f1fd44adc7b6549a368f1168cb3cc
Lauren McCoy <lmccoy8911@gmail.com>
2019-10-06 00:35:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    binding.pry 
+    #binding.pry 
     if !user 
       redirect '/failure' 
     elsif user && user.authenticate(params[:password])
