6de37e1f1a78c89e9896a07612d8b68ae65ff4ab
Lauren McCoy <lmccoy8911@gmail.com>
2019-10-06 01:34:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    if !user 
+    if user && user.authenticate(params[:password]) 
       redirect '/failure'
     else 
       redirect '/account'
